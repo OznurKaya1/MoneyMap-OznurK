73063f4747e52a275e86b395fa2daeac50eaa53d
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-11-07 11:26:07 -0600

    Initial React project files

---
+++ MoneyMap/src/App.jsx
@@ -0,0 +1,10 @@
+import React from'react'
+import WelcomePage from './components/WelcomePage'
+
+export default function App(){
+    return(
+        <div>
+            <WelcomePage></WelcomePage>
+        </div>
+    )
+}
