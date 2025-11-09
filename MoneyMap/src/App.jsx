f7bcff9401eedd3cdfc19887d050251e5203fcc1
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-11-09 13:49:01 -0600

    Add button structure on homepage

--- MoneyMap/src/App.jsx
+++ MoneyMap/src/App.jsx
@@ -1,10 +1,13 @@
-import React from'react'
-import HomePage from './components/HomePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
 
-export default function App(){
-    return(
-        <div>
-            <HomePage></HomePage>
-        </div>
-    )
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
