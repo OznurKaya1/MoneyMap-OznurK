5cabab68fc056f115d4cd0c7dee41c5bc1dedb12
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-11-10 16:27:35 -0600

    Initial Sign up page

--- MoneyMap/src/App.jsx
+++ MoneyMap/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+
     </Routes>
   );
 }
