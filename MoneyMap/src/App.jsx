a3a0729142b0e71904973a98a3da87fa17e6be7e
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-11-10 16:27:35 -0600

    header, footer, navMenu

--- MoneyMap/src/App.jsx
+++ MoneyMap/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import Homepage from './components/HomePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Header from "./components/layout/Header";
+import Footer from './components/layout/Footer'
+import './components/layout/layout.css';
+import './components/layout/Footer';
 
 
-export default function App(){
-    return(
-        <div>
-            <Homepage />
-        </div>
-    )
+export default function App() {
+  return (
+    <div>
+    <Header />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+
+    </Routes>
+    <Footer />
+    </div>
+  );
 }
--- MoneyMap/src/components/HomePage.jsx
+++ MoneyMap/src/components/HomePage.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import './homePage.css'
 import background from './Images/background.jpg'
-import IconImage from './Images/IconImage.jpeg'
-import Logo from './Images/Logo.png'
+
+import { Link } from 'react-router-dom'
 
 export default function HomePage() {
   return (
-    <div className="home-page"
+    <main>
+      <div className="home-page"
 
-      style={{ backgroundImage: `url(${background})` }}
-      
-    >
-      <img src={Logo} className='home-icon' alt='icon' />
-      <h1>MoneyMap</h1>
-      <h3>Map Your Wealth</h3>
-    </div>
-  )
+        style={{ backgroundImage: `url(${background})` }}
+
+      >
+
+        <h1>MoneyMap</h1>
+        <h3>Map Your Wealth</h3>
+        <Link to='/login'>
+          <button className='round-button'> Get Started</button>
+        </Link>
+      </div>
+      </main>
+      )
 }
