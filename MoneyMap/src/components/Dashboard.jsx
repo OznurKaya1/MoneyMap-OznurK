b516afaeac968b81b794b67259c97cb611daf10f
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-11-24 10:20:51 -0600

    Revised CSS and Dasboard

--- MoneyMap/src/components/Dashboard.jsx
+++ MoneyMap/src/components/Dashboard.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import income from './Images/income.jpg'
 import expense from './Images/expense.jpg'
-
+import DashboardCard from './DashboardCard'
 
 export default function Dashboard() {
   return (
@@ -10,17 +9,18 @@ export default function Dashboard() {
       <h2>Take control of your money</h2>
 
       <div className="dashboard-links">
-        <Link to='/income' className="dashboard-link">
-          <span>My Income</span>
-          <img src={income} alt='income' className='dashboard-icon' />
-        </Link>
-
-        <Link to='/expenses' className="dashboard-link">
-          <span>My Expenses</span>
-          <img src={expense} alt='expense' className='dashboard-icon' />
-        </Link>
-
-    
+        <DashboardCard
+          to="/income"
+          label="My Income"
+          img={income}
+          alt="income"
+        />
+        <DashboardCard
+          to="/expenses"
+          label="My Expenses"
+          img={expense}
+          alt="expense"
+        />
       </div>
     </div>
   )
